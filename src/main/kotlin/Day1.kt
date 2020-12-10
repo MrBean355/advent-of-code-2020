d46560495360c25d20cf6bdc35dee44520844c5e
Mike Johnston <mrbean355@gmail.com>
2020-12-10 21:47:22 +0200

    Implement day 1 part 2 code

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -2,12 +2,17 @@
 
 fun main() {
     val values = input.sorted()
+    part1(values)
+    part2(values)
+}
+
+private fun part1(values: List<Int>) {
 
     for (i in 0 until values.size - 1) {
         for (j in i + 1 until values.size) {
             val sum = values[i] + values[j]
             if (sum == 2020) {
-                println("Solution found:")
+                println("Part 1 solution found:")
                 println("${values[i]} * ${values[j]} = ${values[i] * values[j]}")
                 return
             }
@@ -16,7 +21,26 @@ fun main() {
             }
         }
     }
-    println("Nothing found :(")
+    println("Nothing found for solution 1 :(")
+}
+
+private fun part2(values: List<Int>) {
+    for (i in 0 until values.size - 2) {
+        for (j in i + 1 until values.size - 1) {
+            for (k in j + 1 until values.size - 2) {
+                val sum = values[i] + values[j] + values[k]
+                if (sum == 2020) {
+                    println("Part 2 solution found:")
+                    println("${values[i]} * ${values[j]} * ${values[k]} = ${values[i] * values[j] * values[k]}")
+                    return
+                }
+                if (sum > 2020) {
+                    break
+                }
+            }
+        }
+    }
+    println("Nothing found for solution 2 :(")
 }
 
 private val input = listOf(
